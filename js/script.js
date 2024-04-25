a86778c24e24a83cf23fc22207231419a68c304e
KINGPRO3656V <167536851+KINGPRO3656V@users.noreply.github.com>
2024-04-25 20:31:59 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 /* ini file js */
+
+let isReverse = false;
+
+function reverse() {
+    let cToF = document.getElementById('c-to-f');
+    let fToC = document.getElementById('f-to-c');
+
+    if (isReverse) {
+        console.log(isReverse);
+        fToC.style.display = "none";
+        cToF.style.display = "block";
+        isReverse = false;
+    } else {
+        // ini tampilan default
+        console.log(isReverse);
+        cToF.style.display = "none";
+        fToC.style.display = "block";
+        isReverse = true;
+    }
+}
+
+function convert() {
+    let celcius = document.getElementById("c-to-f-input").value;
+    if (!isNaN (/[^1-4]/g)) {
+        alert("Tolong input Nomor Saja");
+    }
+    console.log(celcius);
+}
+
+//document.getElementById("button-convert").addEventListener('click', () => convert());
+/* ini file js */
